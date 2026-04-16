31ec20804bac56ba2ecfb5b5f890c6b4866aafdb
albertozurita-code <albertozurita@correo.ugr.es>
2026-04-16 16:00:27 +0200

    Add files via upload

---
+++ Prácticas_2/Ejercicio1/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/alberto/snap/flutter/common/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/alberto/Carrera/3º/2º/DS/ejercicio3_p2"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
